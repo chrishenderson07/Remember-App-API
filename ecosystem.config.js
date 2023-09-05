c4734f0c9c2543861ceb0922bb87987568ae8f8b
chrishenderson07 <chris.hend012@gmail.com>
2023-09-05 08:12:10 -0300

    pm2 adjusts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,12 @@ module.exports = {
 	apps: [
 		{
 			name: 'app',
-			script: './src/server.js',
+			script: './src/server.js', // Por exemplo, 'app.js'
+			watch: true,
+			ignore_watch: ['node_modules', 'logs'],
 			instances: 'max',
+			exec_mode: 'cluster',
 			env: {
-				NODE_ENV: 'development',
-			},
-			env_production: {
 				NODE_ENV: 'production',
 			},
 		},
